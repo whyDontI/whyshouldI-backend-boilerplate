633d7bc30dcc03530c4468a9ed9d1b51da1065d2
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-07-27 11:32:31 +0530

    Resonse util function refactor

--- util/response.util.js
+++ util/response.util.js
@@ -1,22 +1,24 @@
 const logger = require('../lib/logger/bunyan')
 
-class SendResponse {
-  errorMsg (req, res, status, message, error, scope) {
-    logger.logResponse(req.id, { status: status, message: message }, status)
-    res.status(status).json({ message: message })
-  };
-
-  successMsg (req, res, status, data, message) {
-    const obj = { status, message, data }
-    if (Array.isArray(data)) { obj.count = data.length }
-    logger.logResponse(req.id, obj, status)
-    res.status(status).json(obj)
-  };
-
-  customMsg (req, res, status, message) {
-    logger.logResponse(req.id, { status: status, message: message }, status)
-    res.status(status).json({ message: message })
-  };
+errorMsg (req, res, status, message, error, scope) {
+  logger.logResponse(req.id, { status: status, message: message }, status)
+  res.status(status).json({ message: message })
 };
 
-module.exports = new SendResponse()
+successMsg (req, res, status, data, message) {
+  const obj = { status, message, data }
+  if (Array.isArray(data)) { obj.count = data.length }
+  logger.logResponse(req.id, obj, status)
+  res.status(status).json(obj)
+};
+
+customMsg (req, res, status, message) {
+  logger.logResponse(req.id, { status: status, message: message }, status)
+  res.status(status).json({ message: message })
+};
+
+module.exports = {
+  errorMsg,
+  successMsg,
+  customMsg
+}
