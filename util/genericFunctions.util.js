44e040cb8753295da649cc49fd7106836aea832e
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-07-29 11:30:48 +0530

    generic function refactor

--- util/genericFunctions.util.js
+++ util/genericFunctions.util.js
@@ -1,23 +1,23 @@
-class GenericFunction {
-  async IDGenerator () {
-    this.length = 5
-    this.timestamp = +new Date()
+async IDGenerator () {
+  this.length = 5
+  this.timestamp = +new Date()
 
-    const _getRandomInt = function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min
-    }
-
-    const ts = this.timestamp.toString()
-    const parts = ts.split('').reverse()
-    let id = ''
-
-    for (var i = 0; i < this.length; ++i) {
-      const index = _getRandomInt(0, parts.length - 1)
-      id += parts[index]
-    }
-
-    return id
+  const _getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
-};
 
-module.exports = new GenericFunction()
+  const ts = this.timestamp.toString()
+  const parts = ts.split('').reverse()
+  let id = ''
+
+  for (var i = 0; i < this.length; ++i) {
+    const index = _getRandomInt(0, parts.length - 1)
+    id += parts[index]
+  }
+
+  return id
+}
+
+module.exports = {
+  IDGenerator
+}
