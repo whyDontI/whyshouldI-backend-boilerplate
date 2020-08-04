3ca908e206c6f405ab43a6d650fa498ac918e018
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-08-04 23:37:02 +0530

    converted to es6 IdValidator

--- middlewares/validators/id.validator.js
+++ middlewares/validators/id.validator.js
@@ -1,24 +1,24 @@
 const __ = require('../../util/response.util')
 const mongoose = require('mongoose')
 
-class IdValidator {
-  /*
-    *isValidId() Validation for mongoose _id
-    *@param {object} req
-    *@param {object} res
-    *@param {Function} next
-    *@param {req.params.id} user _id
-    *@return {undefined}
-    * */
-  async isValidId (req, res, next) {
-    try {
-      const result = await mongoose.Types.ObjectId.isValid(req.params.id)
-      if (!result) return __.errorMsg(req, res, 400, 'Incorrect Id provided')
-      return next()
-    } catch (error) {
-      __.errorMsg(req, res, 400, 'Incorrect Id provided', error)
-    };
+/*
+ *isValidId() Validation for mongoose _id
+ *@param {object} req
+ *@param {object} res
+ *@param {Function} next
+ *@param {req.params.id} user _id
+ *@return {undefined}
+ * */
+async function isValidId (req, res, next) {
+  try {
+    const result = await mongoose.Types.ObjectId.isValid(req.params.id)
+    if (!result) return __.errorMsg(req, res, 400, 'Incorrect Id provided')
+    return next()
+  } catch (error) {
+    __.errorMsg(req, res, 400, 'Incorrect Id provided', error)
   };
 };
 
-module.exports = new IdValidator()
+module.exports = {
+  isValidId
+}
